864f90eb30eadff5c5294070041e7f178f18233d
Peter Kubasov <kubasov@gmail.com>
2023-12-13 18:47:46 +0300

    nuxt-config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import {resolve} from "path";
+import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
 	alias: {
